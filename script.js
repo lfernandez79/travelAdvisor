fac574bf4e234f73c1f43cf81440c8f982594583
LeoFdz <lfernandez79@github.com>
2020-02-06 10:19:34 -0600

    api call airport timer works as expected

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // ======================================== Currency converter API ============================================-=
 
 var settings = {
@@ -47,3 +48,42 @@ var settings = {
 $.ajax(settings).done(function (response) {
     console.log(response);
 });
+=======
+// ============================================ AIRPORT FINDER =================================================
+
+var theCity;
+
+// BUTTON GRAB VALUE FROM INPUT #CITYNAME
+$("#airportBtn").on("click", function (event) {
+  // event.preventDefault();
+  $("ul").empty();
+  theCity = $("#cityName").val();
+  
+  // AJAX CALL ==========================================
+  var settings = {
+    url: "https://cometari-airportsfinder-v1.p.rapidapi.com/api/airports/by-text?text=" + theCity,
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "cometari-airportsfinder-v1.p.rapidapi.com",
+      "x-rapidapi-key": "912f8882abmsh597d3316e2e61bcp121db1jsnabc830ace42d",
+    }
+  };
+  $.ajax(settings).done(function (response) {
+  console.log(response);
+    
+
+  // CREATE A UL TO APPEND LIs UNDER, LATER
+  var $newUl = $("<ul>")
+  
+  response.forEach(airport =>  {
+
+    // CREATE LIs TO GRAB INFO FROM ARRAY AND APPENTO UL
+      var $newLi = $(`<li class="text-info">${airport.name} / ${airport.code}</li>`); 
+      $newLi.appendTo($newUl)
+      
+      });
+ $newUl.appendTo("#airportNames")
+
+    }); 
+  })
+>>>>>>> Stashed changes
