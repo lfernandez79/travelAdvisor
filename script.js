f32b4075d6dbc8aa2795d05903dba0fb7ed63db3
LeoFdz <lfernandez79@github.com>
2020-02-06 11:28:48 -0600

    added URL for CORS

--- script.js
+++ script.js
@@ -1,49 +1,39 @@
-// ======================================== Currency converter API ============================================-=
+// ============================================ AIRPORT FINDER =================================================
 
-var settings = {
+var theCity;
+
+// BUTTON GRAB VALUE FROM INPUT #CITYNAME
+$("#airportBtn").on("click", function (event) {
+  // event.preventDefault();
+  $("ul").empty();
+  theCity = $("#cityName").val();
+  
+  // AJAX CALL ==========================================
+  var settings = {
     "async": true,
     "crossDomain": true,
-    "url": "https://currency-converter5.p.rapidapi.com/currency/historical/2018-02-09?format=json&to=AUD&from=AUD&amount=1",
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-host": "currency-converter5.p.rapidapi.com",
-        "x-rapidapi-key": "912f8882abmsh597d3316e2e61bcp121db1jsnabc830ace42d"
+    url: "https://cors-anywhere.herokuapp.com/https://cometari-airportsfinder-v1.p.rapidapi.com/api/airports/by-text?text=" + theCity,
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "cometari-airportsfinder-v1.p.rapidapi.com",
+      "x-rapidapi-key": "912f8882abmsh597d3316e2e61bcp121db1jsnabc830ace42d",
     }
-}
+  };
+  $.ajax(settings).done(function (response) {
+  console.log(response);
+    
 
-$.ajax(settings).done(function (response) {
-    console.log(response);
-});
+  // CREATE A UL TO APPEND LIs UNDER, LATER
+  var $newUl = $("<ul>")
+  
+  response.forEach(airport =>  {
 
-// ============================================ Airport finder =================================================
-var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://cometari-airportsfinder-v1.p.rapidapi.com/api/airports/by-text?text=Atlanta",
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-host": "cometari-airportsfinder-v1.p.rapidapi.com",
-        "x-rapidapi-key": "912f8882abmsh597d3316e2e61bcp121db1jsnabc830ace42d"
-    }
-}
+    // CREATE LIs TO GRAB INFO FROM ARRAY AND APPENTO UL
+      var $newLi = $(`<li class="text-info">${airport.name} / ${airport.code}</li>`); 
+      $newLi.appendTo($newUl)
+      
+      });
+ $newUl.appendTo("#airportNames")
 
-$.ajax(settings).done(function (response) {
-    console.log(response);
-});
-
-// ============================================= World Time =========================================================
-
-var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://world-time2.p.rapidapi.com/timezone/Europe/London",
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-host": "world-time2.p.rapidapi.com",
-        "x-rapidapi-key": "912f8882abmsh597d3316e2e61bcp121db1jsnabc830ace42d"
-    }
-}
-
-$.ajax(settings).done(function (response) {
-    console.log(response);
-});
+    }); 
+  })
