005e4d6123fb3a0646c62b98a10e85c7fa77aafe
LeoFdz <lfernandez79@github.com>
2020-02-07 11:40:05 -0600

    all added style, re-formatted text. Fixed APis

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var theCity;
 
 // BUTTON GRAB VALUE FROM INPUT #CITYNAME
 $("#airportBtn").on("click", function(event) {
-	// event.preventDefault();
+	event.preventDefault();
 	$("ul").empty();
 	theCity = $("#cityName").val();
 
@@ -29,7 +29,7 @@ $("#airportBtn").on("click", function(event) {
 		response.forEach(airport => {
 			// CREATE LIs TO GRAB INFO FROM ARRAY AND APPENTO UL
 			var $newLi = $(
-				`<li class="text-info">${airport.name} / ${airport.code}</li>`
+				`<li class="list-unstyled text-seconday">${airport.name} / ${airport.code}</li>`
 			);
 
 			$newLi.appendTo($newUl);
@@ -65,25 +65,25 @@ $("#clockSearchBtn").on("click", function(e) {
         var $clockUl = $("<ul>");
 
         var $clockLi = $(
-            `<li class="text-info">${
-                response.timezone
-            }</li><li class="text-info">${
-                // moment().format(date)
+          `<li class="list-unstyled text-secondary">${
+            response.timezone
+          }</li><li class="list-unstyled text-secondary">${
+            // moment().format(date)
 
-                // var date = response.datetime;
+            // var date = response.datetime;
 
-                response.datetime
+            response.datetime
 
-                // .format(
+            // .format(
 
-                //  "dddd, MMMM Do YYYY, h:mm:ss a"
+            //  "dddd, MMMM Do YYYY, h:mm:ss a"
 
-                // )
+            // )
 
-                // .utc()
+            // .utc()
 
-                // .format("dddd, MMMM Do YYYY, h:mm a")
-            }</li>`
+            // .format("dddd, MMMM Do YYYY, h:mm a")
+          }</li>`
         );
 
         console.log(
@@ -132,16 +132,14 @@ $("#convertBtn").on("click", function(event) {
 		console.log(response);
 
 			var $moneyUl = $("<ul>");
-			var $moneyli = $(`<li class="text-info">${response.rates[cur2].rate_for_amount}</li>`)
+			var $moneyli = $(`<li class="list-unstyled text-secondary">${response.rates[cur2].rate_for_amount}</li>`
+      );
 		
 			console.log(response.rates[cur2].rate_for_amount);
 
 			$moneyli.appendTo($moneyUl);
 			$moneyUl.appendTo("#currencyConv");
 		});
-			
-		
-
 		
 	console.log(amount);
 });
